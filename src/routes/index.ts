b833187e95370fb271f38c346cc3f80e861f596e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-11-09 23:53:05 +0900

    ソフトウェアキーボードがレイアウトを崩壊させる件について調査する準備

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from "vue-router"
 export const routePaths = {
   root: "/",
   focus: "/focus",
+  softwareKeyboardLayout: "/keyboard_layout",
 }
 
 const routes = [
   { name: "root", path: routePaths.root, component: () => import("@/views/RootView.vue") },
   { name: "focus", path: routePaths.focus, component: () => import("@/views/FocusView.vue") },
+  { name: "layout", path: routePaths.softwareKeyboardLayout, component: () => import("@/views/SoftwereKeyboardLayoutView.vue") },
 ]
 
 export const tabItems = routes.map(route => {
---
+++ src/views/SoftwereKeyboardLayoutView.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+/**
+ * ～このViewで取り扱う問題～
+ * ソフトウェアキーボードの開閉によってレイアウトが崩れる事がある
+ * 
+ * ～問題の設定～
+ * 再現条件調査中
+ */
+import { onMounted, onBeforeUnmount } from "vue"
+
+/**
+ * リサイズされた
+ */
+const onResize = () => {
+}
+
+onMounted(() => {
+  window.addEventListener("resize", onResize)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener("resize", onResize)
+})
+</script>
+
+<template lang="pug">
+div
+  .root
+    .title ソフトウェアキーボードがレイアウトを壊す
+</template>
+
+<style lang="sass" scoped>
+.root
+  width: 100%
+  height: 100%
+  
+  .title
+    text-align: center
+    font-size: 18px
+</style>
