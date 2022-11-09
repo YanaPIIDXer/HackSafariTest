be7a404477bb499d8ed1eeaa9595ff4c2f9b3ebc
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-11-09 21:21:33 +0900

    PrimeVueの設定

--- src/main.ts
+++ src/main.ts
@@ -9,4 +9,16 @@ const app = createApp(App)
 import router from "./routes"
 app.use(router)
 
+// PrimeVue
+import "primevue/resources/themes/saga-blue/theme.css"
+import "primevue/resources/primevue.min.css"
+import "primeicons/primeicons.css"
+import "primeflex/primeflex.css"
+import PrimeVue from 'primevue/config'
+import PrimeButton from "primevue/button"
+import InputText from "primevue/inputtext"
+app.use(PrimeVue)
+app.component("PrimeButton", PrimeButton)
+app.component("InputText", InputText)
+
 app.mount('#app')
--- src/views/RootView.vue
+++ src/views/RootView.vue
@@ -1,15 +1,23 @@
 <script setup lang="ts">
+/**
+ * 許すまじボタンが押された
+ */
+const onClickYurusumaji = () => {
+  alert("アップル許すまじ")
+}
 </script>
 
 <template lang="pug">
 div
   .root
     .message アップル許すまじ
+    PrimeButton(@click="onClickYurusumaji") 許すまじ
 </template>
 
 <style lang="sass" scoped>
 .root
+  text-align: center
+  
   .message
-    text-align: center
     font-size: 64px
 </style>
