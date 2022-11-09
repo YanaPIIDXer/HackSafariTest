f70c41534ee022d8fd96457652a131e44993893d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-11-09 21:35:04 +0900

    ダブルクリックによってfocus()メソッドを実行するテスト用のView作成 ※問題の概要についてはコメント参照

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from "vue-router"
 
 export const routePaths = {
   root: "/",
+  focus: "/focus",
 }
 
 const routes = [
   { name: "root", path: routePaths.root, component: () => import("@/views/RootView.vue") },
+  { name: "focus", path: routePaths.focus, component: () => import("@/views/FocusView.vue") },
 ]
 
 export const tabItems = routes.map(route => {
---
+++ src/views/FocusView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+/**
+ *        ～このViewで取り扱う問題～
+ * 特定の要素にフォーカスを移すためのfocusメソッドが、
+ * Safariだと特定のイベント内でないと機能しない
+ * 
+ *             ～問題の設定～
+ *    特定の要素をダブルクリックした時に
+ *    特定の要素に対してフォーカスを合わせる
+ *    ※dblclickイベント内ではfocus使用不可
+ */
+</script>
+
+<template lang="pug">
+div
+  .root
+    .title ダブルクリックによるfocus()実行
+</template>
+
+<style lang="sass" scoped>
+.root
+  .title
+    text-align: center
+    font-size: 18px
+</style>
