173fb83c05ed0a7c2da19f747fa8efe2c2ccefba
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-11-10 00:19:45 +0900

    現象再現

--- src/views/SoftwereKeyboardLayoutView.vue
+++ src/views/SoftwereKeyboardLayoutView.vue
@@ -3,19 +3,42 @@
  * ～このViewで取り扱う問題～
  * ソフトウェアキーボードの開閉によってレイアウトが崩れる事がある
  * 
- * ～問題の設定～
- * 再現条件調査中
+ * ～発生している現象～
+ * ・heightをvh単位で設定していると、ソフトウェアキーボードが出てきた時に1vh毎のサイズに変動が生じ（？）、スクロールが走る
+ * 　→vhの高さが変動する？
+ * ・ソフトウェアキーボードが消えても1vh毎のサイズは元には戻らないし、スクロールで元に戻る事もない
+ * ・この時touchmoveでpreventDefaultしてスワイプによるスクロールを抑制していると、当然画面外に飛んだ要素は帰ってこなくなる
  */
-import { onMounted, onBeforeUnmount } from "vue"
+import { ref, onMounted, onBeforeUnmount } from "vue"
+
+const width = ref(100)
+const height = ref(100)
 
 /**
  * リサイズされた
  */
 const onResize = () => {
+  width.value = window.innerWidth * 0.3
+  height.value = window.innerHeight * 0.3
+}
+
+/**
+ * 自動フォーカスアウト
+ */
+const autoFocusOut = (e: Event) => {
+  setTimeout(() => {
+    const target = e.target as HTMLElement
+    target.blur()
+  }, 1000)
+}
+
+const ignoreScroll = (e: Event) => {
+  e.preventDefault()
 }
 
 onMounted(() => {
   window.addEventListener("resize", onResize)
+  onResize()
 })
 
 onBeforeUnmount(() => {
@@ -25,16 +48,28 @@ onBeforeUnmount(() => {
 
 <template lang="pug">
 div
-  .root
+  .root(@touchmove="ignoreScroll")
     .title ソフトウェアキーボードがレイアウトを壊す
+    .target(:style="{ width: `${width}px`, height: `${height}px` }")
+    textarea.textarea(@focus="autoFocusOut")
 </template>
 
 <style lang="sass" scoped>
 .root
-  width: 100%
-  height: 100%
+  width: 100vw
+  height: 100vh
   
   .title
     text-align: center
     font-size: 18px
+
+  .target
+    background: #000000
+    margin: 0 auto
+
+  .textarea
+    position: absolute
+    bottom: 0
+    width: 100%
+    font-size: 17px
 </style>
