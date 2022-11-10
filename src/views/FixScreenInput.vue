ce251b60e60986204fd3385c1d4ea4fe57c60e5a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-11-10 20:42:22 +0900

    スクロールもズームも発生してしまう入力

--- src/views/FixScreenInput.vue
+++ src/views/FixScreenInput.vue
@@ -4,20 +4,29 @@
  * テキストフォームにフォーカスを当てた時に走る
  * 画面のスクロールやズームを抑制する
  */
+
 </script>
 
 <template lang="pug">
 div
   .root
     .title inputにフォーカスを当てた時のスクロール・ズームを抑制
+    PrimeButton フォーカスを外す
+    textarea.rawInput(style="left: 300px; top: 400px" placeholder="スクロールもズームも発生する")
 </template>
 
 <style lang="sass" scoped>
 .root
   width: 100%
   height: 100%
+  position: relative
   
   .title
     text-align: center
     font-size: 18px
+
+  .rawInput
+    position: absolute
+    resize: none
+    font-size: 24px
 </style>
