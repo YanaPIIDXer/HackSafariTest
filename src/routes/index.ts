003001a6799c3b9f464b93dc40fa2617da6a7a00
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-11-10 20:34:55 +0900

    次の問題（inputにフォーカスを当てた時のスクロール・ズーム抑制）

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,12 +4,14 @@ export const routePaths = {
   root: "/",
   focus: "/focus",
   softwareKeyboardLayout: "/keyboard_layout",
+  fixScreenInput: "/input",
 }
 
 const routes = [
   { name: "root", path: routePaths.root, component: () => import("@/views/RootView.vue") },
   { name: "focus", path: routePaths.focus, component: () => import("@/views/FocusView.vue") },
   { name: "layout", path: routePaths.softwareKeyboardLayout, component: () => import("@/views/SoftwereKeyboardLayoutView.vue") },
+  { name: "input", path: routePaths.fixScreenInput, component: () => import("@/views/FixScreenInput.vue") },
 ]
 
 export const tabItems = routes.map(route => {
---
+++ src/views/FixScreenInput.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+/**
+ * ～このViewで取り扱う問題～
+ * テキストフォームにフォーカスを当てた時に走る
+ * 画面のスクロールやズームを抑制する
+ */
+</script>
+
+<template lang="pug">
+div
+  .root
+    .title inputにフォーカスを当てた時のスクロール・ズームを抑制
+</template>
+
+<style lang="sass" scoped>
+.root
+  width: 100%
+  height: 100%
+  
+  .title
+    text-align: center
+    font-size: 18px
+</style>
