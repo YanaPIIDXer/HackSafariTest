65de1d993e92579bd5b5071e216f3d138f7340b9
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-11-10 20:49:18 +0900

    実装用Component

---
+++ src/components/HackTextArea.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps({
+  placeholder: {
+    type: String,
+    required: false,
+    default: "",
+  }
+})
+</script>
+
+<template lang="pug">
+span
+  span.root
+    textarea.main(ref="mainRef" :placeholder="props.placeholder")
+    textarea.trigger(ref="triggerRef" :placeholder="props.placeholder")
+</template>
+
+<style lang="sass" scoped>
+.root
+  textarea
+    resize: none
+</style>
--- src/views/FixScreenInput.vue
+++ src/views/FixScreenInput.vue
@@ -4,7 +4,7 @@
  * テキストフォームにフォーカスを当てた時に走る
  * 画面のスクロールやズームを抑制する
  */
-
+import HackTextArea from "@/components/HackTextArea.vue"
 </script>
 
 <template lang="pug">
@@ -12,7 +12,8 @@ div
   .root
     .title inputにフォーカスを当てた時のスクロール・ズームを抑制
     PrimeButton フォーカスを外す
-    textarea.rawInput(style="left: 300px; top: 400px" placeholder="スクロールもズームも発生する")
+    textarea.rawInput(style="left: 200px; top: 400px" placeholder="スクロールもズームも発生する")
+    HackTextArea.hackInput(style="left: 350px; top: 200px" placeholder="画面固定")
 </template>
 
 <style lang="sass" scoped>
@@ -28,5 +29,9 @@ div
   .rawInput
     position: absolute
     resize: none
+    font-size: 24px
+
+  .hackInput
+    position: absolute
     font-size: 24px
 </style>
